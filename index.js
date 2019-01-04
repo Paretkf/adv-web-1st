77174fc4ea6891cfd8fd22db88a54436562406f0
Paretkf <paretk7@gmail.com>
2019-01-04 14:28:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+let users = [
+  {
+    "id": 1000,
+    "name": "Corey Griffith",
+    "username": "coGriffith",
+    "gender": "male",
+    "age": 13
+  },
+  {
+    "id": 1001,
+    "name": "Marion McDaniel",
+    "username": "mMcDaniel",
+    "gender": "female",
+    "age": 15
+  },
+  {
+    "id": 1002,
+    "name": "Tom Robbins",
+    "username": "tRobbins",
+    "gender": "male",
+    "age": 15
+  },
+  {
+    "id": 1003,
+    "name": "Ruth Harvey",
+    "username": "rHarvey",
+    "gender": "female",
+    "age": 14
+  },
+  {
+    "id": 1004,
+    "name": "Terry Willis",
+    "username": "tWillis",
+    "gender": "male",
+    "age": 13
+  }
+]
+
+// let myFunc = function () {
+//   console.log('myFunc')
+// }
+
+// function a () {
+//   console.log('a')
+// }
+
+// let b = () => {
+//   console.log('b')
+// }
+
+// myFunc()
+// a()
+// b()
+
+// function main () {
+//   plusNumber1(5, 10, function(result) {
+//     console.log(result) // 15
+//   })
+//   console.log(plusNumber2(3,7))
+// }
+// function plusNumber1(num1, num2, callback) {
+//   callback(num1+num2)
+// }
+// function plusNumber2(num1, num2) {
+//   return num1+num2
+// }
+// main() // log 15 จากนั้น log 10
+
+setTimeout(function () {
+  console.log(`first`)
+ }, 100)
+ console.log(`second`) //log second ก่อนแล้ว log first
